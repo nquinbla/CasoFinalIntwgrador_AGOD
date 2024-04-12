8a00d58da54b3259994fbf9f47158e526333b3db
nquinbla <nquinbla@myuax.com>
2024-04-12 14:14:44 +0200

    gestión de fechas

--- src/main/java/GestiónInfoCientífica/GestiónFechas.java
+++ src/main/java/GestiónInfoCientífica/GestiónFechas.java
@@ -3,6 +3,7 @@ package GestiónInfoCientífica;
 import java.util.List;
 
 public class GestiónFechas {
+    // Método que recibe una lista de fechas y devuelve la misma lista
     public static List<String> obtenerFechas(List<String> fechas) {
         return fechas;
     }
