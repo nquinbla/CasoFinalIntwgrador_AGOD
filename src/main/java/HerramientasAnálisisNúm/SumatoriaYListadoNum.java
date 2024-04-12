7f4d48b245adf7ffcd36224d563495cc0e9cfc46
nquinbla <nquinbla@myuax.com>
2024-04-12 13:17:31 +0200

    tengo 9 errores de no se de donde 2

--- src/main/java/HerramientasAnálisisNúm/SumatoriaYListadoNum.java
+++ src/main/java/HerramientasAnálisisNúm/SumatoriaYListadoNum.java
@@ -1,19 +1,24 @@
 package HerramientasAnálisisNúm;
 
 public class SumatoriaYListadoNum {
-    // Función recursiva para calcular la sumatoria de los números de 1 a n.
-    public static int sumNúmeros(int n) {
-        if (n == 0) { // Caso base
+    // Método para calcular la sumatoria de los números desde 1 hasta n
+    public static int calcularSumatoria(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("El número debe ser no negativo, es decir, positivo.");
+        } if (n == 0) { // Caso base: si n es 0, devuelve 0
             return 0;
-        } else { // Caso recursivo
-            return n + sumNúmeros(n - 1);
+        } else { // Caso recursivo: devuelve n más la sumatoria de n - 1
+            return n + calcularSumatoria(n - 1);
         }
     }
 
-    public static void listNúmeros(int start, int end) {
-        if (start <= end) {
-            System.out.println(start + "");
-            listNúmeros(start + 1, end);
+    // Método para listar los números desde el inicio hasta el final
+    public static void listarNumeros(int inicio, int end) {
+        if (inicio > final) { // Si el inicio es mayor al final, lanza una excepción
+            throw new IllegalArgumentException("El inicio debe ser menor o igual al final.");
+        } if (inicio <=final) { // Caso base: si el inicio es menor o igual al final, imprime el inicio y llama al método con inicio + 1
+            System.out.println(inicio);
+            listarNumeros(inicio + 1, end);
         }
     }
 }
