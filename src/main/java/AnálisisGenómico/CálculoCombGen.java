cedd01f29b90fc3cbc5ec2ae03fa64f1e0a8c872
nquinbla <nquinbla@myuax.com>
2024-04-12 12:58:02 +0200

    CálculoCombGen 1

--- src/main/java/AnálisisGenómico/CálculoCombGen.java
+++ src/main/java/AnálisisGenómico/CálculoCombGen.java
@@ -1,4 +1,13 @@
 package AnálisisGenómico;
 
 public class CálculoCombGen {
+    public static int calcularCombGen (int n) {
+        if (n == 0) {
+            return 1;
+        } else if (n == 1) {
+            return 2;
+        } else {
+            return 2 * calcularCombGen(n - 1);
+        }
+    }
 }
