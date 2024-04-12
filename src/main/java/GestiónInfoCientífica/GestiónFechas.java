eaa43a5e5a21c81e42603ecdf5c4a838a8e0095e
nquinbla <nquinbla@myuax.com>
2024-04-12 14:13:53 +0200

    búsqueda en textos comentarios

--- src/main/java/GestiónInfoCientífica/GestiónFechas.java
+++ src/main/java/GestiónInfoCientífica/GestiónFechas.java
@@ -1,4 +1,10 @@
 package GestiónInfoCientífica;
 
+import java.util.List;
+
 public class GestiónFechas {
+    public static List<String> obtenerFechas(List<String> fechas) {
+        return fechas;
+    }
+
 }
