8da60cabc03688586c20fd71e3438dc0e7cf181f
nquinbla <nquinbla@myuax.com>
2024-04-12 12:47:06 +0200

    conteo genes 1

--- src/main/java/AnálisisGenómico/ConteoGenes.java
+++ src/main/java/AnálisisGenómico/ConteoGenes.java
@@ -1,4 +1,20 @@
 package AnálisisGenómico;
 
 public class ConteoGenes {
+    public static int contarGenes(String genes) {
+        return contarGenesHelper("dna", 0,0);
+    }
+    private static int contarGenesHelper(String dna, int index, int count) {
+        if (index >= dna.length() -2 ) {
+            return count;
+        } if (dna.startsWith("ATG", index)) {
+            int endIndex = findStopCodon(dna, index + 3);
+            if (endIndex != -1) {
+                count++;
+                return contarGenesHelper(dna, endIndex + 3, count);
+            }
+        } return contarGenesHelper(dna, index + 1, count);
+     }
+     private static int
+
 }
