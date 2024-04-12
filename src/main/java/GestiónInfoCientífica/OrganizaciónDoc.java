2d62c35f63bb7e0b03700330ce4a5b5ff6149da3
nquinbla <nquinbla@myuax.com>
2024-04-12 14:04:16 +0200

    organización de documnetos código

--- src/main/java/GestiónInfoCientífica/OrganizaciónDoc.java
+++ src/main/java/GestiónInfoCientífica/OrganizaciónDoc.java
@@ -1,4 +1,16 @@
 package GestiónInfoCientífica;
 
 public class OrganizaciónDoc {
+    public static String[] organizarAlfabéticamente(String[] documentos) {
+        for (int i = 0; i < documentos.length; i++) {
+            for (int j = i + 1; j < documentos.length; j++) {
+                if (documentos[i].compareTo(documentos[j]) > 0) {
+                    String temp = documentos[i];
+                    documentos[i] = documentos[j];
+                    documentos[j] = temp;
+                }
+            }
+        }
+        return documentos;
+    }
 }
