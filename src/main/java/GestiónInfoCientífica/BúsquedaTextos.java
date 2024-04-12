2ba78f6db9c05d511babc25c9c1396cb0928d921
nquinbla <nquinbla@myuax.com>
2024-04-12 14:10:55 +0200

    búsqueda en textos comentarios

--- src/main/java/GestiónInfoCientífica/BúsquedaTextos.java
+++ src/main/java/GestiónInfoCientífica/BúsquedaTextos.java
@@ -1,28 +1,30 @@
 package GestiónInfoCientífica;
 
 public class BúsquedaTextos {
+    // Método que busca un texto en un array de textos
     public static int búsquedaLineal (String[] array, String texto) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i].equals(texto)) {
-                return i;
+        for (int i = 0; i < array.length; i++) { // Recorremos el array
+            if (array[i].equals(texto)) { // Si encontramos el texto
+                return i; // Devolvemos la posición
             }
         }
         return -1;
     }
 
+    // Método que busca un texto en un array de textos ordenado
     public static int búsquedaBinaria (String[] array, String texto) {
         int inicio = 0;
         int fin = array.length - 1;
-        while (inicio <= fin) {
-            int medio = (inicio + fin) / 2;
-            if (array[medio].equals(texto)) {
-                return medio;
-            } else if (array[medio].compareTo(texto) < 0) {
-                inicio = medio + 1;
-            } else {
-                fin = medio - 1;
+        while (inicio <= fin) { // Mientras que el inicio sea menor o igual que el final
+            int medio = (inicio + fin) / 2; // Calculamos el punto medio
+            if (array[medio].equals(texto)) { // Si encontramos el texto
+                return medio; // Devolvemos la posición
+            } else if (array[medio].compareTo(texto) < 0) { // Si el texto está en la mitad derecha
+                inicio = medio + 1; // Actualizamos el inicio
+            } else { // Si el texto está en la mitad izquierda
+                fin = medio - 1; // Actualizamos el final
             }
         }
-        return -1;
+        return -1; // Si no encontramos el texto, devolvemos -1
     }
 }
