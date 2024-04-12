eb7dc494763bfeeea28f9e1553900eb82ea3078d
nquinbla <nquinbla@myuax.com>
2024-04-12 13:04:14 +0200

    main módulo 1

--- src/main/java/AnálisisGenómico/Main_AG.java
+++ src/main/java/AnálisisGenómico/Main_AG.java
@@ -1,16 +1,29 @@
 package AnálisisGenómico;
 
+import java.util.Scanner;
 
 public class Main_AG {
     public static void main(String[] args) {
-        // Ejemplo de uso de ConteoGenes
-        String cadenaADN = "ATGCTGATTAAATGCGATGATGCTAA";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("---Análisis Genómico ---");
+
+        // Solicitar al usuario la cadena de ADN
+        System.out.println("Por favor, introduzca la cadena de ADN:");
+        String cadenaADN = scanner.nextLine();
+
+        // Solicitar al usuario el número de genes
+        System.out.println("Por favor, introduzca el número de genes:");
+        int n = scanner.nextInt();
+
+        // Calcular y mostrar el número de genes en la cadena de ADN
         int genesContados = ConteoGenes.contarGenes(cadenaADN);
         System.out.println("Número de genes en la cadena de ADN: " + genesContados);
 
-        // Ejemplo de uso de GeneticCombinationCalculator
-        int n = 3; // Número de genes
+        // Calcular y mostrar el número de combinaciones genéticas posibles
         int combinaciones = CálculoCombGen.calcularCombGen(n);
         System.out.println("Número de combinaciones genéticas posibles para " + n + " genes: " + combinaciones);
+
+        scanner.close();
     }
 }
