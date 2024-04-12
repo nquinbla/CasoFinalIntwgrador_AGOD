df4d4a34d04d218ac500358fa7e2cb5d0d4fdd11
nquinbla <nquinbla@myuax.com>
2024-04-12 12:58:31 +0200

    CálculoCombGen comentarios

--- src/main/java/AnálisisGenómico/CálculoCombGen.java
+++ src/main/java/AnálisisGenómico/CálculoCombGen.java
@@ -1,12 +1,13 @@
 package AnálisisGenómico;
 
+// Clase pública para calcular el número de combinaciones genéticas
 public class CálculoCombGen {
     public static int calcularCombGen (int n) {
-        if (n == 0) {
+        if (n == 0) { // Si n es 0, devuelve 1
             return 1;
-        } else if (n == 1) {
+        } else if (n == 1) { // Si n es 1, devuelve 2
             return 2;
-        } else {
+        } else { // Si n es mayor que 1, devuelve 2 * calcularCombGen(n - 1)
             return 2 * calcularCombGen(n - 1);
         }
     }
