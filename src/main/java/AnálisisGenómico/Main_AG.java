5c276e429655038d5904b1df2c79aa2a301c3477
nquinbla <nquinbla@myuax.com>
2024-04-12 13:04:59 +0200

    main módulo 1 (ajustes)

--- src/main/java/AnálisisGenómico/Main_AG.java
+++ src/main/java/AnálisisGenómico/Main_AG.java
@@ -8,19 +8,15 @@ public class Main_AG {
 
         System.out.println("---Análisis Genómico ---");
 
-        // Solicitar al usuario la cadena de ADN
         System.out.println("Por favor, introduzca la cadena de ADN:");
         String cadenaADN = scanner.nextLine();
 
-        // Solicitar al usuario el número de genes
         System.out.println("Por favor, introduzca el número de genes:");
         int n = scanner.nextInt();
 
-        // Calcular y mostrar el número de genes en la cadena de ADN
         int genesContados = ConteoGenes.contarGenes(cadenaADN);
         System.out.println("Número de genes en la cadena de ADN: " + genesContados);
 
-        // Calcular y mostrar el número de combinaciones genéticas posibles
         int combinaciones = CálculoCombGen.calcularCombGen(n);
         System.out.println("Número de combinaciones genéticas posibles para " + n + " genes: " + combinaciones);
 
