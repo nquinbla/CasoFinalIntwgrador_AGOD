92fdc33dbe709eed3ff2e4dda2d946936e6c65ff
nquinbla <nquinbla@myuax.com>
2024-04-12 14:16:00 +0200

    ajustes gestión de fechasp

--- src/main/java/GestiónInfoCientífica/GestiónFechas.java
+++ src/main/java/GestiónInfoCientífica/GestiónFechas.java
@@ -1,11 +1,50 @@
 package GestiónInfoCientífica;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class GestiónFechas {
-    // Método que recibe una lista de fechas y devuelve la misma lista
-    public static List<String> obtenerFechas(List<String> fechas) {
-        return fechas;
+    private List<Fecha> fechas;
+
+    public GestiónFechas() {
+        this.fechas = new ArrayList<>();
     }
 
+    public void agregarFecha(int dia, int mes, int año) {
+        this.fechas.add(new Fecha(dia, mes, año));
+    }
+
+    public List<Fecha> listarFechas() {
+        Collections.sort(this.fechas);
+        return this.fechas;
+    }
+
+    private class Fecha implements Comparable<Fecha> {
+        private int dia;
+        private int mes;
+        private int año;
+
+        public Fecha(int dia, int mes, int año) {
+            this.dia = dia;
+            this.mes = mes;
+            this.año = año;
+        }
+
+        @Override
+        public int compareTo(Fecha otraFecha) {
+            if (this.año != otraFecha.año) {
+                return this.año - otraFecha.año;
+            } else if (this.mes != otraFecha.mes) {
+                return this.mes - otraFecha.mes;
+            } else {
+                return this.dia - otraFecha.dia;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return dia + "/" + mes + "/" + año;
+        }
+    }
 }
