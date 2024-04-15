e553a2e82cb79cdedd366a15e643bb38cc08ab25
nquinbla <nquinbla@myuax.com>
2024-04-15 20:56:13 +0200

    arreglillos para que furulen bien las cosas 2

--- src/main/java/AnálisisGenómico/ConteoGenes.java
+++ src/main/java/AnálisisGenómico/ConteoGenes.java
@@ -1,18 +1,25 @@
 package AnálisisGenómico;
 
 public class ConteoGenes {
-    public static int contarGenes(String cadenaADN) {
-        if (cadenaADN.length() < 3) {
-            return 0;
-        }
-        if (cadenaADN.startsWith("ATG")) {
-            int endIndex = cadenaADN.indexOf("TAA", 3);
-            if (endIndex != -1 && (endIndex - 3) % 3 == 0) {
-                return 1 + contarGenes(cadenaADN.substring(endIndex + 3));
+    public static int contarGenes(String dna) {
+        int count = 0;
+        int startIndex = 0;
+        while (true) {
+            int startCodon = dna.indexOf("ATG", startIndex);
+            if (startCodon == -1) {
+                break;
+            }
+            int stopCodon = dna.indexOf("TAA", startCodon + 3);
+            if (stopCodon != -1 && (stopCodon - startCodon) % 3 == 0) {
+                count++;
+                startIndex = stopCodon + 3;
+            } else {
+                startIndex = startCodon + 3;
             }
         }
-        return contarGenes(cadenaADN.substring(1));
+        return count;
     }
+
 
     private static int contarGenesRecursivamente(String cadenaADN, int index, int count) {
         if (index >= cadenaADN.length() -2 ) {
