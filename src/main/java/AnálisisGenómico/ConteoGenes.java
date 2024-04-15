2a4d3dfffdc85d0ee108c7b024f5760fb8b88598
nquinbla <nquinbla@myuax.com>
2024-04-15 22:35:20 +0200

    arreglos conteo de genes

--- src/main/java/AnálisisGenómico/ConteoGenes.java
+++ src/main/java/AnálisisGenómico/ConteoGenes.java
@@ -1,22 +1,25 @@
 package AnálisisGenómico;
 public class ConteoGenes {
-    public static int contarGenes(String dna) {
-        return contarGenesRecursivamente(dna, 0);
-    }
-
-    public static int contarGenesRecursivamente(String dna, int startIndex) {
-        if (startIndex >= dna.length() - 2) {
+    public static int contarGenes(String dna){
+        if (dna.isEmpty()){
             return 0;
         }
-        int startCodon = dna.indexOf("ATG", startIndex);
-        if (startCodon != -1) {
-            int stopCodon = dna.indexOf("TAA", startCodon + 3);
-            if (stopCodon != -1 && (stopCodon - startCodon) % 3 == 0) {
-                return 1 + contarGenesRecursivamente(dna, stopCodon + 3);
-            } else {
-                return contarGenesRecursivamente(dna, startCodon + 3);
+        int index = dna.indexOf("ATG");
+        if (index == -1){
+            return 0;
+        }
+        return 1 + contarGenes(dna.substring(index + 3));
+    }
+    public static int contarGenesRecursivamente(String dna, int index){
+        if (index >= dna.length()){
+            return 0;
+        }
+        if (dna.startsWith("ATG", index)){
+            int fin = dna.indexOf("TAG", index);
+            if ((fin - index) % 3 == 0){
+                return 1 + contarGenesRecursivamente(dna, fin + 3);
             }
         }
-        return contarGenesRecursivamente(dna, startIndex + 1);
+        return contarGenesRecursivamente(dna, index + 1);
     }
 }
