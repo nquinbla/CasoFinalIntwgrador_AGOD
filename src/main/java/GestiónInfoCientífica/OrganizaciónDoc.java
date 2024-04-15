92fc9e2aa627c282b1826d7f55d3f1926979fbf6
nquinbla <nquinbla@myuax.com>
2024-04-15 12:50:57 +0200

    cosas que borre sin darme cuenta 1

---
+++ src/main/java/GestiónInfoCientífica/OrganizaciónDoc.java
@@ -0,0 +1,16 @@
+package GestiónInfoCientífica;
+
+public class OrganizaciónDoc {
+    // Organiza los documentos alfabéticamente
+    public static String[] organizarAlfabéticamente(String[] documentos) {
+        for (int i = 0; i < documentos.length; i++) { // Ordena los documentos
+            for (int j = i + 1; j < documentos.length; j++) { // Si el documento i es mayor al documento j, se intercambian
+                if (documentos[i].compareTo(documentos[j]) > 0) { // Compara los documentos
+                    String temp = documentos[i];
+                    documentos[i] = documentos[j];
+                    documentos[j] = temp;
+                }
+            }
+        } return documentos;
+    }
+}
